5eb34a6139dd2734099c982fb90806fb32d1803d
kshitijgithb <101198295+kshitijgithb@users.noreply.github.com>
2023-05-23 00:32:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,36 +4,23 @@ import pandas as pd
 import numpy as np
 import os
 import pickle
-import warnings
-
 warnings.filterwarnings("ignore", message="Trying to unpickle estimator")
 
 st.set_page_config(page_title="Green Delight", page_icon="https://i.pinimg.com/736x/8f/0e/88/8f0e8808d45745a3e0e7fefc21b452dd.jpg", layout='centered', initial_sidebar_state="collapsed")
 
-
-com.html("""
-<div>
-<this is my first HTml page
-<button> This is a button </button>
-</div>
-""")
-
-
+import warnings
 def load_model(modelfile):
 	loaded_model = pickle.load(open(modelfile, 'rb'))
 	return loaded_model
-
+    
 def main():
-    # title
     html_temp = """
     <div>
     <h1 style="color:GREEN;text-align:center;"> Choose your Crop With Green Delight </h1>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-
     col = st.columns(1)[0]
-
     with col:
         st.subheader("Get best Crop to make your Farm Flourish")
         N = st.number_input("Nitrogen", 1,10000)
@@ -46,9 +33,7 @@ def main():
 
         feature_list = [N, P, K, temp, humidity, ph, rainfall]
         single_pred = np.array(feature_list).reshape(1,-1)
-        
         if st.button('Explore'):
-
             loaded_model = load_model('model.pkl')
             prediction = loaded_model.predict(single_pred)
             col.write('''
@@ -61,7 +46,6 @@ def main():
     #MainMenu {visibility: hidden;}
     </style>
     """
-
 hide_menu_style = """
         <style>
         .block-container {padding: 2rem 1rem 3rem;}
@@ -69,6 +53,5 @@ hide_menu_style = """
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
-
 if __name__ == '__main__':
 	main()
